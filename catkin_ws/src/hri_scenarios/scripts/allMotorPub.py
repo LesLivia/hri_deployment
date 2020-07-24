6ff3126721cecf9564ba90bafe2285119a2f0ac4
LesLivia <livia.lestingi@polimi.it>
2020-07-24 12:13:15 +0200

    added all motors speed publisher

---
+++ catkin_ws/src/hri_scenarios/scripts/allMotorPub.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import rospy
+import sys
+from std_msgs.msg import Float32
+
+def allMotorPub(speed):
+    pub = rospy.Publisher('allMotorSpeed', Float32, queue_size=10)
+    rospy.init_node('allPub', anonymous=False)
+    rospy.loginfo(speed)
+    rate = rospy.Rate(10)
+    count = 0
+    while count < 3:
+	pub.publish(speed) 
+        rate.sleep()
+        count+=1
+
+if __name__ == '__main__':
+    try:
+	print(sys.argv)
+        allMotorPub(float(sys.argv[1]))
+    except rospy.ROSInterruptException:
+        pass
---
+++ main.sh
@@ -0,0 +1,2 @@
+rosrun hri_scenarios leftMotorPub.py
+rosrun hri_scenarios rightMotorPub.py
