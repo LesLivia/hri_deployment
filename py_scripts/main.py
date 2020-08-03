6ceafa8d378b38111539efe2298a4cb3b6e8700a
LesLivia <livia.lestingi@polimi.it>
2020-08-03 12:25:59 +0200

    modified folder name

--- py_scripts/main.py
+++ py_scripts/main.py
@@ -33,10 +33,7 @@ try:
 	
 	time.sleep(1)
 	dest = Point(5.0, 3.2)
-	rob.turn_left(1.57)
-	time.sleep(0.5)
-	rob.turn_right(-1.57)
-	#rob.navigate_to(dest)
+	rob.navigate_to(dest)
 
 	rob.set_sim_running(0)
 	bill.set_sim_running(0)
