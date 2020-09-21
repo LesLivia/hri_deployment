4c7a940edc0a884a4dede1b424a19405ba92e584
LesLivia <livia.lestingi@polimi.it>
2020-09-21 16:00:09 +0200

    added support for multiple humans

--- py_scripts/agents/human.py
+++ py_scripts/agents/human.py
@@ -2,6 +2,7 @@
 import os
 import rospy_utils.hrirosnode as hriros
 import rospy_utils.hriconstants as const
+from typing import List
 from multiprocessing import Pool
 from agents.position import Position
 from agents.coordinates import Point
@@ -39,82 +40,86 @@ class Human:
 	def is_moving(self):
 		return self.moving
 
-	def start_reading_data(self):
-		f = open('../scene_logs/humanPosition.log', 'r+')
-		f.truncate(0)
+def start_reading_data(humans: List[Human]):
+	f = open('../scene_logs/humanPosition.log', 'r+')
+	f.truncate(0)
 
-		node = 'humSensorsSub.py'
+	node = 'humSensorsSub.py'
 
-		pool = Pool()
-		pool.starmap(hriros.rosrun_nodes, [(node, '')])
+	pool = Pool()
+	pool.starmap(hriros.rosrun_nodes, [(node, '')])
 
-		f = open('../scene_logs/humanFatigue.log', 'r+')
-		f.truncate(0)
+	f = open('../scene_logs/humanFatigue.log', 'r+')
+	f.truncate(0)
 
-		node = 'humFtgSub.py'
+	node = 'humFtgSub.py'
 
-		pool = Pool()
-		pool.starmap(hriros.rosrun_nodes, [(node, '')])
+	pool = Pool()
+	pool.starmap(hriros.rosrun_nodes, [(node, '')])
 
-		f = open('../scene_logs/humansServed.log', 'r+')
-		f.truncate(0)
+	f = open('../scene_logs/humansServed.log', 'r+')
+	f.truncate(0)
 
-		node = 'humServiceSub.py'
+	node = 'humServiceSub.py'
 
-		pool = Pool()
-		pool.starmap(hriros.rosrun_nodes, [(node, '')])
+	pool = Pool()
+	pool.starmap(hriros.rosrun_nodes, [(node, '')])
 
-		self.set_sim_running(1)
+	for hum in humans:	
+		hum.set_sim_running(1)
 
-	def follow_position(self):
-		filename = '../scene_logs/humanPosition.log'
-		_cached_stamp = 0
-		_cached_pos = None
-		while self.is_sim_running():
-			try:
-				stamp = os.stat(filename).st_mtime
-				if stamp != _cached_stamp:
-					f = open(filename, 'r')
-					lines = f.read().splitlines()
-					last_line = lines[-1]
-					if self.get_position():
-						_cached_pos = self.get_position()
-					new_position = Position.parse_position(last_line)
-					new_position.x += const.VREP_X_OFFSET
-					new_position.y += const.VREP_Y_OFFSET
+def follow_position(hums: List[Human]):
+	filename = '../scene_logs/humanPosition.log'
+	_cached_stamp = 0
+	_cached_pos = None
+	_last_read_line = 1
+	while hums[0].is_sim_running():
+		stamp = os.stat(filename).st_mtime
+		if stamp != _cached_stamp:
+			f = open(filename, 'r')
+			lines = f.read().splitlines()
+			new_lines = lines[_last_read_line:]
+			for line in new_lines:
+				humId = int((line.split(':')[0]).replace('hum', ''))
+				hum = hums[humId-1]
+				pos_str = line.split(':')[1]
+				if hum.get_position():
+					_cached_pos = hum.get_position()
+				new_position = Position.parse_position(pos_str)
+				new_position.x += const.VREP_X_OFFSET
+				new_position.y += const.VREP_Y_OFFSET
 
-					self.set_position(new_position)
+				hum.set_position(new_position)
 
-					if _cached_pos is not None:
-						_cached_pt = Point(_cached_pos.x, _cached_pos.y)
-						new_pt = Point(new_position.x, new_position.y)
-						if self.is_moving() and new_pt.distance_from(_cached_pt) < 0.5:
-							print('Human is idle')
-							self.set_is_moving(False)
-						elif not self.is_moving() and new_pt.distance_from(_cached_pt) >= 0.5:
-							print('Human is moving')
-							self.set_is_moving(True)
-					_cached_stamp = stamp
-			except (KeyboardInterrupt, SystemExit):
-				print('Stopping human position monitoring...')
-				return
+				if _cached_pos is not None:
+					_cached_pt = Point(_cached_pos.x, _cached_pos.y)
+					new_pt = Point(new_position.x, new_position.y)
+					if hum.is_moving() and new_pt.distance_from(_cached_pt) < 0.5:
+						print('Human is idle')
+						hum.set_is_moving(False)
+					elif not hum.is_moving() and new_pt.distance_from(_cached_pt) >= 0.5:
+						print('Human is moving')
+						hum.set_is_moving(True)
 
-	def follow_fatigue(self):
-		filename = '../scene_logs/humanFatigue.log'
-		_cached_stamp = 0
-		while self.is_sim_running():
-			try:
-				stamp = os.stat(filename).st_mtime
-				if stamp != _cached_stamp:
-					f = open(filename, 'r')
-					lines = f.read().splitlines()
-					last_line = lines[-1]
-					new_ftg = float(last_line)
+			_cached_stamp = stamp
+			_last_read_line = len(lines)-1
 
-					self.set_fatigue(new_ftg)
-					_cached_stamp = stamp
-			except (KeyboardInterrupt, SystemExit):
-				print('Stopping human fatigue monitoring...')
-				return
 
+def follow_fatigue(hums: List[Human]):
+	filename = '../scene_logs/humanFatigue.log'
+	_cached_stamp = 0
+	_last_read_line = 1
+	while hums[0].is_sim_running():
+		stamp = os.stat(filename).st_mtime
+		if stamp != _cached_stamp:
+			f = open(filename, 'r')
+			lines = f.read().splitlines()
+			new_lines = lines[_last_read_line:]
+			for line in new_lines:
+				humId = int((line.split(':')[0]).replace('hum', ''))
+				hum = hums[humId-1]
+				new_ftg = float(line.split(':')[1])
+				hum.set_fatigue(new_ftg)
+			_cached_stamp = stamp
+			_last_read_line = len(lines)-1
 
--- py_scripts/main.py
+++ py_scripts/main.py
@@ -8,7 +8,7 @@ import agents.navigation as nav
 from threading import Thread
 from multiprocessing import Pool
 from agents.mobilerobot import MobileRobot
-from agents.human import Human
+from agents.human import Human, start_reading_data, follow_position, follow_fatigue
 from agents.coordinates import Point
 from agents.orchestrator import Orchestrator
 from agents.mission import *
@@ -19,24 +19,25 @@ vrep_sim = vrep.connect(19999)
 vrep.start_sim(vrep_sim)
 
 bill = Human(1, 10, 1, 1)
+alice = Human(2, 10, 1, 1)
 rob = MobileRobot(1, 15.0, 5.0)
 
-dest = [Point(22.0, 15.0)]
-humans = [bill]
+dest = [Point(22.0, 15.0), None]
+humans = [bill, alice]
 
-patterns = [Pattern.HUM_FOLLOWER]		
+patterns = [Pattern.HUM_FOLLOWER, Pattern.HUM_LEADER]		
 mission = Mission(patterns, dest)	
 
 try:
 	# START ROS NODES THAT ACQUIRE DATA FROM SENSORS
-	bill.start_reading_data()
+	start_reading_data(humans)
 	rob.start_reading_data()
 	time.sleep(7)
 	
 	# START MONITORING HUMAN SENSOR DATA LOGS
-	thread_h = Thread(target = bill.follow_position)
+	thread_h = Thread(target = follow_position, args=[humans])
 	thread_h.start()
-	thread_h_f = Thread(target = bill.follow_fatigue)
+	thread_h_f = Thread(target = follow_fatigue, args=[humans])
 	thread_h_f.start()
 	
 	# START MONITORING ROBOT SENSOR DATA LOGS
@@ -62,7 +63,8 @@ try:
 
 	# when mission is over (either with success or failure),
 	# shut everything down
-	bill.set_sim_running(0)
+	for hum in humans:
+		hum.set_sim_running(0)
 	vrep.stop_sim(vrep_sim)
 	print('Execution finished.')
 	quit()
