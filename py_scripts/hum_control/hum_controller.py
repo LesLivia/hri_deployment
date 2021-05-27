fc3ab3aafdf6712b483e9b5b0f457553eed3a0a9
LesLivia <livia.lestingi@polimi.it>
2021-05-27 18:20:58 +0200

    wip automated human

--- py_scripts/hum_control/hum_controller.py
+++ py_scripts/hum_control/hum_controller.py
@@ -2,27 +2,61 @@
 import time
 import vrep_utils.vrep as vrep
 from typing import List
+from enum import Enum
 from agents.human import Human, FatigueProfile
 from agents.coordinates import Point
 from agents.orchestrator import Orchestrator, OpChk
 from agents.mission import *
 
+class Loc(Enum):
+	INIT = -1
+	IDLE = 0
+	BUSY = 1
+	PASSED_OUT = 99
+
+	def __str__(self):
+		if self == Loc.INIT:
+			return 'INIT'
+		elif self == Loc.IDLE:
+			return 'IDLE'
+		elif self == Loc.BUSY:
+			return 'BUSY'
+		else:
+			return 'PASSED_OUT'
+
 class HumanController:
-	def __init__(self, h: List[Human], clientID):
+	def __init__(self, h: List[Human], clientID, debug: bool = False):
 		self.clientID = clientID
 		self.h = h
 		self.currH = 0
+		self.Tpoll = 1.0
+		self.served = [False]*len(h)
+		self.debug = debug
+		# SHA-graph variables
+		self.LOC = Loc.INIT
+
+	def set_loc(self, loc: Loc):
+		if self.debug:
+			print('SHA: switching from {} to {}'.format(str(self.LOC), str(loc)))
+		self.loc = loc
 
 	def start_h_action(self):
+		self.set_loc(Loc.BUSY)
 		vrep.start_human(self.clientID, self.h[self.currH].hum_id)
 
 	def stop_h_action(self):
+		self.set_loc(Loc.IDLE)
 		vrep.stop_human(self.clientID, self.h[self.currH].hum_id)
 
 	def run_follower(self):
-		self.start_h_action()
-		time.sleep(10)
-		self.stop_h_action()
+		self.set_loc(Loc.IDLE)
+		while not self.served[self.currH]:
+			self.start_h_action()
+			time.sleep(10)
+			self.stop_h_action()
+			self.served[self.currH] = True
+		print('Human {} successfully served'.format(self.h[self.currH].hum_id))
+		self.currH+=1
 
 	def run_leader(self):
 		pass
--- py_scripts/hum_main.py
+++ py_scripts/hum_main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 import time
 import vrep_utils.vrep as vrep
 from agents.mobilerobot import MobileRobot
@@ -8,20 +9,26 @@ from agents.mission import *
 from hum_control.hum_controller import HumanController
 
 vrep_sim = vrep.connect(19999)
+if vrep_sim == -1:
+	# connection to v-rep could not be established
+	quit()
+
 vrep.start_sim(vrep_sim)
 
 bill = Human(1, 10, FatigueProfile.YOUNG_SICK, 1)
 carl = Human(2, 10, FatigueProfile.ELDERLY_HEALTHY, 1)
 rob = MobileRobot(1, 8.0, 5.0)
 
-# Alternative A
 dest = [Point(24.0, 10.5)]
 unique_humans = [bill]
 humans = [bill, bill]
 patterns = [Pattern.HUM_FOLLOWER]	
 
 mission = Mission(patterns, dest)	
-contr = HumanController(humans, vrep_sim)
+
+
+debug = bool(sys.argv[1]) if len(sys.argv)>1 else False
+contr = HumanController(humans, vrep_sim, debug)
 
 try:
 	time.sleep(7)
