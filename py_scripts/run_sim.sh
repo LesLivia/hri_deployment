441c6f1a982b91a0d84825bb2b7896ddd33c840f
LesLivia <livia.lestingi@polimi.it>
2021-05-28 15:55:16 +0200

    automated sim script

---
+++ py_scripts/run_sim.sh
@@ -0,0 +1,10 @@
+python3 main.py > outfile.txt &
+sleep 2
+python3 hum_main.py
+NEW_DIR=SIM_"$(date +"%Y-%m-%d-%H-%M-%S")"
+mkdir /media/psf/logs/sim_logs/$NEW_DIR
+cp ../scene_logs/humanFatigue.log /media/psf/logs/sim_logs/$NEW_DIR/
+cp ../scene_logs/humanPosition.log /media/psf/logs/sim_logs/$NEW_DIR/
+cp ../scene_logs/humansServed.log /media/psf/logs/sim_logs/$NEW_DIR/
+cp ../scene_logs/robotPosition.log /media/psf/logs/sim_logs/$NEW_DIR/
+cp ../scene_logs/robotBattery.log /media/psf/logs/sim_logs/$NEW_DIR/
