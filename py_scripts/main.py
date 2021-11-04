b2a4a25f0417813fbff3e0588e17c1f737fc22f1
LesLivia <livia.lestingi@polimi.it>
2021-11-04 17:11:13 +0100

    minor fix to bash script to run missions

--- py_scripts/main.py
+++ py_scripts/main.py
@@ -44,7 +44,7 @@ bill = Human(1, 10, FatigueProfile.YOUNG_SICK, 1)
 carl = Human(2, 10, FatigueProfile.ELDERLY_HEALTHY, 1)
 rob = MobileRobot(1, 12.0, 5.0)
 
-dest = [Point(22.0, 10.0)]#, Point(22.0, 4.0)]
+dest = [Point(22.0, 15.0)]#, Point(22.0, 4.0)]
 unique_humans = [bill]
 humans = [bill]#, bill]
 
--- py_scripts/run_sim.sh
+++ py_scripts/run_sim.sh
@@ -1,4 +1,4 @@
-for i in {1..1}
+for i in {1..$2}
 do
 	# python3 randomize_mission.py
 	gnome-terminal -e "python3 main.py"
@@ -6,13 +6,13 @@ do
 	sleep 15
 	python3 hum_main.py
 	NEW_DIR=SIM_"$(date +"%Y-%m-%d-%H-%M-%S")"
-	mkdir /media/psf/logs/sim_logs/$NEW_DIR
-	cp outfile.txt /media/psf/logs/sim_logs/$NEW_DIR/
-	cp ../scene_logs/humanFatigue.log /media/psf/logs/sim_logs/$NEW_DIR/
-	cp ../scene_logs/humanPosition.log /media/psf/logs/sim_logs/$NEW_DIR/
-	cp ../scene_logs/humansServed.log /media/psf/logs/sim_logs/$NEW_DIR/
-	cp ../scene_logs/robotPosition.log /media/psf/logs/sim_logs/$NEW_DIR/
-	cp ../scene_logs/robotBattery.log /media/psf/logs/sim_logs/$NEW_DIR/
-	cp ../scene_logs/environmentData.log /media/psf/logs/sim_logs/$NEW_DIR/
+	mkdir /media/psf/logs/sim_logs/faoc_exp/$1/$NEW_DIR
+	cp outfile.txt /media/psf/logs/sim_logs/faoc_exp/$1/$NEW_DIR/
+	cp ../scene_logs/humanFatigue.log /media/psf/logs/sim_logs/faoc_exp/$1/$NEW_DIR/
+	cp ../scene_logs/humanPosition.log /media/psf/logs/sim_logs/faoc_exp/$1/$NEW_DIR/
+	cp ../scene_logs/humansServed.log /media/psf/logs/sim_logs/faoc_exp/$1/$NEW_DIR/
+	cp ../scene_logs/robotPosition.log /media/psf/logs/sim_logs/faoc_exp/$1/$NEW_DIR/
+	cp ../scene_logs/robotBattery.log /media/psf/logs/sim_logs/faoc_exp/$1/$NEW_DIR/
+	cp ../scene_logs/environmentData.log /media/psf/logs/sim_logs/faoc_exp/$1/$NEW_DIR/
 	sleep 10
 done
